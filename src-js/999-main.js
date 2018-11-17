c195aee6f90cecf5e412fc658083249b427bc0ea
Chris Oakman <chris.oakman@gmail.com>
2018-11-17 14:06:14 -0600

    fix the build

--- src-js/999-main.js
+++ src-js/999-main.js
@@ -4,6 +4,7 @@ window.MYAPP = window.MYAPP || {}
 ;(function () {
   // NOTE: referencing "$" inside this module prevents standard from yelling at us
   var $ = window.jQuery
+  var MYAPP = window.MYAPP
 
   function globalInit () {
     console.info('Intializing the application now.')
