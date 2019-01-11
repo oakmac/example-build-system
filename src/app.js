2216c8b0b0147d67baef64493b3b6c66f0301753
Lanny Wang <lanzhenwang9@gmail.com>
2019-01-11 06:26:04 +0000

    set up the html template with ejs

--- src/app.js
+++ src/app.js
@@ -9,6 +9,47 @@ app.get('/', function(req, res){
 
 app.get('/movies', function(req, res){
   //this is the main movies page on which the users can search their movies
+  var data=[{
+  "movieid": 1,
+  "movie": "Jupiter's Wife",
+  "movie genre": "Documentary"
+}, {
+  "movieid": 2,
+  "movie": "Far",
+  "movie genre": "Drama"
+}, {
+  "movieid": 3,
+  "movie": "Homeboy",
+  "movie genre": "Drama|Romance"
+}, {
+  "movieid": 4,
+  "movie": "America's Heart and Soul",
+  "movie genre": "Documentary"
+}, {
+  "movieid": 5,
+  "movie": "Frozen River",
+  "movie genre": "Drama"
+}, {
+  "movieid": 6,
+  "movie": "Stand by Me Doraemon",
+  "movie genre": "Animation|Children|Drama|Fantasy"
+}, {
+  "movieid": 7,
+  "movie": "Dream (Bi-mong)",
+  "movie genre": "Drama|Fantasy|Mystery|Romance"
+}, {
+  "movieid": 8,
+  "movie": "Liar's Dice",
+  "movie genre": "Adventure|Children|Drama"
+}, {
+  "movieid": 9,
+  "movie": "Inside Deep Throat",
+  "movie genre": "Documentary"
+}, {
+  "movieid": 10,
+  "movie": "Prisoner of Paradise",
+  "movie genre": "Documentary"
+}]
   res.render('movies')
 })
 app.get('/results', function (req, res) {
@@ -20,8 +61,46 @@ app.get('/cart', function (req, res) {
   res.render('cart')
 })
 
+
 app.get('/user', function (req, res) {
   //will use REStful api here
+  var user = [{
+  "id": 1,
+  "user_name": "Manda",
+  "email": "mshrubb0@wordpress.org",
+  "name": "Manda Shrubb",
+  "address": "859 Marcy Place"
+}, {
+  "id": 2,
+  "user_name": "Carmelia",
+  "email": "cgunny1@ow.ly",
+  "name": "Carmelia Gunny",
+  "address": "1 Crescent Oaks Trail"
+}, {
+  "id": 3,
+  "user_name": "Rancell",
+  "email": "rligoe2@wp.com",
+  "name": "Rancell Ligoe",
+  "address": "715 Commercial Place"
+}, {
+  "id": 4,
+  "user_name": "Nickie",
+  "email": "nbarnaby3@ebay.co.uk",
+  "name": "Nickie Barnaby",
+  "address": "0379 Golden Leaf Park"
+}, {
+  "id": 5,
+  "user_name": "Adriana",
+  "email": "abroad4@engadget.com",
+  "name": "Adriana Broad",
+  "address": "7375 Green Trail"
+}, {
+  "id": 6,
+  "user_name": "Collete",
+  "email": "cpendreigh5@foxnews.com",
+  "name": "Collete Pendreigh",
+  "address": "99 Main Plaza"
+}]
   res.render('user')
 })
 
