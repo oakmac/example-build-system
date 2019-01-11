52f284edf9c07168447769aae76a771a8956113b
Lanny Wang <lanzhenwang9@gmail.com>
2019-01-11 01:20:20 +0000

    created basic ejs views

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const app = express()
 // const request = require('request')
-// app.set('view engine', 'ejs')
-app.get('/main', function (req, res) {
-  res.render('search')
+app.set('view engine', 'ejs')
+app.get('/', function(req, res){
+  //this is the landing page
+  res.render('landing')
 })
+
+app.get('/movies', function(req, res){
+  //this is the main movies page on which the users can search their movies
+  res.render('movies')
+})
+app.get('/results', function (req, res) {
+  //this is the page to show the result of what the user have searched.
+  res.render('results')
+})
+
+app.get('/cart', function (req, res) {
+  res.render('cart')
+})
+
+app.get('/user', function (req, res) {
+  //will use REStful api here
+  res.render('user')
+})
+
+
+
 // app.get('/results', function (req, res) {
 //   var query = req.query.search
 //     var url = 'http://www.omdbapi.com/?s=' + query + '&apikey=thewdb'
